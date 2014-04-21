14de9664fb2e6acd201b91d77b15bd49eccada88
davidpearce <davidpearce2@gmail.com>
2014-04-21 17:47:04 +0800

    Added iotop service data and interface

--- interfaces/default/js/services.js
+++ interfaces/default/js/services.js
@@ -1,8 +1,19 @@
 // Document ready
+
+
+
+// Set timeintercal to refresh stats
+setInterval(function () {
+    //get_diskinfo();
+    reloadtab();
+}, 10000);
+
+
+
+
 $(document).ready(function () {
     $('.spinner').show();
 });
-
 
 function get_watchedshows() {
     $.get(WEBDIR + 'services/outputwatchedplex', function (response) {
@@ -15,3 +26,18 @@ function get_iotop() {
         $('#iotop').html(response);
     });
 }
+
+function reloadtab() {
+    if ($('#watched').is(':visible')) {
+        get_watchedshows();
+    } else if ($('#iotop').is(':visible')) {
+        get_iotop();
+    }
+}
+
+$('#watched').click(function () {
+    get_watchedshows();
+});
+$('#iotop').click(function () {
+    get_iotop();
+});
