69f32c1d3f0d4d57f34f26f1fa0e19da773c45b3
davidpearce <davidpearce2@gmail.com>
2014-04-21 17:33:49 +0800

    Added iotop service data

--- modules/services.py
+++ modules/services.py
@@ -70,7 +70,7 @@ class Services:
     def getiotop(self):
         import subprocess
         temp = subprocess.check_output("iotop -o -n 1 -b", shell=True)
-        #temp2 = temp.replace("\n", "<br />")
+        #temp2 = temp.replace("\n", "</table><br/><table>")
         temp2 = "<table border='1'><tr><td>" + re.sub(' +',' ',temp).replace(" ", "</td><td>") + "</td></tr></table>"
         output = temp2.replace("\n", "<br/>").replace("<br/></td>", "</td></tr><tr>").replace("</td><td>M/s", "M/s").replace("</td><td>K/s","K/s").replace("</td><td>B/s","B/s").replace("DISK</td><td>WRITE", "Disk Write").replace("DISK</td><td>READ", "Disk Read").replace("</td><td>%", "%").replace("Total</td><td>Disk", "Total Disk")
         return output
